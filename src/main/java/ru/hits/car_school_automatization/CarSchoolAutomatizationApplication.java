7572b4ff017e72e3757b83862d2057d52cee904b
Березовский Семён <ber2semen@gmail.com>
2026-02-13 10:47:16 +0700

    Initial commit

---
+++ src/main/java/ru/hits/car_school_automatization/CarSchoolAutomatizationApplication.java
@@ -0,0 +1,13 @@
+package ru.hits.car_school_automatization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSchoolAutomatizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSchoolAutomatizationApplication.class, args);
+    }
+
+}
