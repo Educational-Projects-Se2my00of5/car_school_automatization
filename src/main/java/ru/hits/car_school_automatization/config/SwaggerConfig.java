79fe36a92dc944a935d77d6a56017b38789af707
Березовский Семён <ber2semen@gmail.com>
2026-02-22 15:38:17 +0700

    security для сваггера. поле для ввода токена

--- src/main/java/ru/hits/car_school_automatization/config/SwaggerConfig.java
+++ src/main/java/ru/hits/car_school_automatization/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package ru.hits.car_school_automatization.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +16,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        
         return new OpenAPI()
                 .info(new Info()
                         .title("Car School Automatization API")
                         .version("1.0.0")
                         .description("REST API для автоматизации автошколы")
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
                 );
     }
 }
